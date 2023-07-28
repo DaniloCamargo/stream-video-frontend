8a2ba5a7c2c46bbc2702a09014c6af186ac804bb
Danilo Camargo <danilo.omnis@gmail.com>
2023-07-28 10:19:38 -0300

    inclusao modal para video

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ReactPlayer from 'react-player';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import { PlayFill } from 'react-bootstrap-icons';
+import Modal from 'react-modal';
 
 interface Video {
   id: number;
@@ -14,9 +15,16 @@ interface Video {
 const App: React.FC = () => {
   const [videos, setVideos] = useState<Video[]>([]);
   const [selectedVideo, setSelectedVideo] = useState<Video | null>(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleVideoClick = (video: Video) => {
     setSelectedVideo(video);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedVideo(null);
+    setModalIsOpen(false);
   };
   
   const playVideo = (videoId: number) => {
@@ -37,13 +45,13 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <Container className="text-center">
-      <div className="bg-purple text-white py-2 my-4">
+    <Container>
+      <div className="bg-purple text-center text-white py-2 my-4">
         <h1 className="">Lista de Vídeos</h1>
       </div>
-      <Row>
+      <Row className='justify-content-center'>
         {videos.map((video) => (
-          <Col key={video.id} md={4} className="mb-3">
+          <Col key={video.id} md={4} className="mb-3 text-center col-3">
             <Card>
               <img src={`http://localhost:3001/images/${video.thumb}`} alt="" />
               <h3 className="card-title">{video.title}</h3>
@@ -54,14 +62,22 @@ const App: React.FC = () => {
           </Col>
         ))}
       </Row>
-      {selectedVideo && (
-        <ReactPlayer
-          url={`http://localhost:3001/video/${selectedVideo.id}`}
-          controls
-          playing
-          className="mt-4"
-        />
-      )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Vídeo"
+      >
+        {selectedVideo && (
+          <ReactPlayer
+            url={`http://localhost:3001/video/${selectedVideo.id}`}
+            controls
+            playing
+            width="100%"
+            height="auto"
+          />
+        )}
+        <button onClick={closeModal}>Fechar</button>
+      </Modal>
     </Container>
   );
 };
